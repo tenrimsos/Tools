d1c3041c0410c2a631b0a8dbc8efc74fdd696f91
tenrimsos <31085027+tenrimsos@users.noreply.github.com>
2025-07-18 22:50:21 -0600

    Update eza.sh

--- eza.sh
+++ eza.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-echo "" >> .bashrc
-echo "alias tree='eza -1laT --icons=always --total-size'" >> .bashrc 
-echo "alias ls='eza'" >> .bashrc
-echo "alias la='eza -a'" >> .bashrc
-echo "alias ld='eza -lD --total-size'" >> .bashrc
-echo "alias lda='eza -lDa --total-size'" >> .bashrc
-echo "alias lf='eza -lf --color=always | grep -v /'" >> .bashrc
-echo "alias lfa='eza -lfa --color=always | grep -v /'" >> .bashrc
-echo "alias ll='eza -lda * .* --group-directories-first --icons=always --total-size'" >> .bashrc
-echo "alias lt='eza -l --sort=modified'" >> .bashrc
-echo "alias lta='eza -la --sort=modified'" >> .bashrc
-echo "alias lsd='eza -lDa --icons=always --absolute=on --group-directories-first --total-size'" >> .bashrc
-echo "alias lsda='eza -la --icons=always --absolute=on --group-directories-first --total-size'" >> .bashrc
+echo "" >> $HOME/.bashrc
+echo "alias tree='eza -1laT --icons=always --total-size'" >> $HOME/.bashrc 
+echo "alias ls='eza'" >> $HOME/.bashrc
+echo "alias la='eza -a'" >> $HOME/.bashrc
+echo "alias ld='eza -lD --total-size'" >> $HOME/.bashrc
+echo "alias lda='eza -lDa --total-size'" >> $HOME/.bashrc
+echo "alias lf='eza -lf --color=always | grep -v /'" >> $HOME/.bashrc
+echo "alias lfa='eza -lfa --color=always | grep -v /'" >> $HOME/.bashrc
+echo "alias ll='eza -lda * .* --group-directories-first --icons=always --total-size'" >> $HOME/.bashrc
+echo "alias lt='eza -l --sort=modified'" >> $HOME/.bashrc
+echo "alias lta='eza -la --sort=modified'" >> $HOME/.bashrc
+echo "alias lsd='eza -lDa --icons=always --absolute=on --group-directories-first --total-size'" >> $HOME/.bashrc
+echo "alias lsda='eza -la --icons=always --absolute=on --group-directories-first --total-size'" >> $HOME/.bashrc
 
 
 sudo wget -O /usr/local/bin/eza "https://github.com/tenrimsos/Tools/raw/refs/heads/main/eza_x64"
